e10a1334c182270a63a1d8d37b830c077899d734
lee <lsljw65@gmail.com>
2024-06-25 17:32:08 +0900

    REST Api 시작 전

---
+++ src/componet/Day.js
@@ -0,0 +1,22 @@
+import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
+import Word from "./Word";
+
+export default function Day() {
+  const a = useParams();
+  const day = a.day;
+
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
+  return (
+    <div>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {wordList.map((word) => (
+            <Word word={word} key={word.id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
